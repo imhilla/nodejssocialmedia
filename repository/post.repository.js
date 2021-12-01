eae91753e8e297493241ce26ffc4ebca324502c3
imhilla <hillaryodhiambo282@gmail.com>
2021-12-01 22:40:38 +0300

    finalize posts repository

--- repository/post.repository.js
+++ repository/post.repository.js
@@ -21,4 +21,25 @@ class PostRepository {
     }
     return post;
   }
+
+  async updatePost(post) {
+    let data = {};
+    try {
+      data = await Post.updateOne(post);
+    } catch (err) {
+      logger.error("Error::" + err);
+    }
+  }
+
+  async deletePost(postId) {
+    let data = {};
+    try {
+      data = await Post.deleteOne({ _id: postId });
+    } catch (err) {
+      logger.error("Error::" + err);
+    }
+    return { status: `${data.deleteCount > 0 ? true : false}` };
+  }
 }
+
+module.exports = new PostRepository();
