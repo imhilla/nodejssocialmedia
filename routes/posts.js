0dc9d0319c906559356bec2d2e3e05b70c465f77
imhilla <hillaryodhiambo282@gmail.com>
2021-12-01 21:24:58 +0300

    add api logger

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,8 @@ router.get("/posts", function (req, res, next) {
   res.send("respond with a all posts");
 });
 
-// router.get("/posts/:1d", function (req, res, next) {
-//   res.send("respond with a single post");
-// });
+router.get("/posts/:1d", function (req, res, next) {
+  res.send("respond with a single post");
+});
 
 module.exports = router;
