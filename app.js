24363755613627f22b3097ea3b8148157aa9bba9
imhilla <hillaryodhiambo282@gmail.com>
2021-12-02 07:15:03 +0300

    make everything work

--- app.js
+++ app.js
@@ -1,40 +1,33 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
 require("dotenv").config();
-const postController = require("./controller/post.controller");
-// const postsRouter = require("./routes/posts");
+
+const postContoller = require("./controller/post.controller");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.use("/api", postsRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use(bodyParser.json());
 
 app.get("/api/posts", (req, res) => {
-  postController.getTasks().then((data) => res.json(data));
+  postContoller.getPosts().then((data) => res.json(data));
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.post("/api/post", (req, res) => {
+  console.log(req.body);
+  postContoller.createPost(req.body.task).then((data) => res.json(data));
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.json("error");
+app.put("/api/post", (req, res) => {
+  postContoller.updatePost(req.body.task).then((data) => res.json(data));
+});
+
+app.delete("/api/post/:id", (req, res) => {
+  postContoller.deletePost(req.params.id).then((data) => res.json(data));
+});
+
+app.get("/", (req, res) => {
+  res.send(`<h1>API Works !!!</h1>`);
 });
 
 app.listen(port, () => {
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-// const postController = require("../controller/post.controller");
+const postController = require("../controller/post.controller");
 /* GET all posts*/
 router.get("/posts", function (req, res, next) {
-  // postController.getTasks().then((data) => res.json(data));
+  postController.getTasks().then((data) => res.json(data));
 });
 
 router.get("/posts/:1d", function (req, res, next) {
